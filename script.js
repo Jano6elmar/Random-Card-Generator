55bab353b8a6d2e9fd8232b953b180d7362b72ff
Jano6elmar <jano6belmar@.com>
2021-01-08 07:24:02 -0400

    tarea completada

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-
-
-    
-    
-   window.onload = () => {
+window.onload = () => {
         document.querySelector('.card').classList.add(generateRandompalo()); 
         document.querySelector('.card').innerHTML = generateRandomNumber()
-    };               /*puede ir el nombre de la class number ahi donde dice .card para generateRandom???*/
+    };               
    
    let generateRandomNumber = () => {
     let n = ["A", "2", "3", "4" ,"5","6","7","8","9","10","J","Q","K"] 
@@ -21,12 +17,4 @@
     };
     
 
-    /*window.onload = () => {
-    let card = [ "A", 2,3,4,5,6,7,8,9,10,"J","Q","K"]
-    let palo = [ 'spade','heart','club','diamond']
     
-    let number = document.querySelector('.number');
-
-    number.innerHTML = card[Math.floor(Math.random()*card.length)];
-    number.classList.add(palo[Math.floor(Math.random()*palo.length)]);
-    }   */
